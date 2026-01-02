699c066775cd05dc9652b04818075897c3a2539d
Your Name <your.email@example.com>
2026-01-03 00:04:51 +0530

    Add home route and templates folder

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,render_template
 from flask_cors import CORS
 import numpy as np
 import pickle
 
 app = Flask(__name__)
 CORS(app)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 # ================= LOAD TRAINED ARTIFACTS =================
 with open("model.pkl", "rb") as f:
